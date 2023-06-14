963c3c2564c26a946da37fe2f023b25e6547fbef
Jackson Zhou <jianchenfutbol@gmail.com>
2023-06-14 23:59:21 +0200

    changes home logo

--- personal-portfolio/src/components/Home/index.js
+++ personal-portfolio/src/components/Home/index.js
@@ -1,24 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './index.scss'
+import Logo from './Logo'
 import { Link } from 'react-router-dom'
+import AnimatedLetters from '../AnimatedLetters'
 
 const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = [" ",'J', 'a', 'c', 'k', 's', 'o', 'n']
+  const jobArray = [
+    'F',
+    'r',
+    'o',
+    'n',
+    't',
+    '-',
+    'E',
+    'n',
+    'd',
+    ' ',
+    'D',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    "."
+  ]
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setLetterClass('text-animate-hover');
+    }, 4000);
+  
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, []);
+  
   return (
-      <div className="container home-page">
-        <div className="text-zone">
-          <h1>
-            Hi, <br /> I'm
-            
-            Jackson
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass}`}>i,</span>
             <br />
-            web developer
-          </h1>
-          <h2>Frontend Developer</h2>
-          <Link to="/contact" className="flat-button">
-            Contact Me
-          </Link>
-        </div>
+            <span className={`${letterClass}`}>I</span>
+            <span className={`${letterClass}`}>'m</span> 
+          <AnimatedLetters letterClass ={letterClass} strArray={nameArray} idx={15} />
+          <br />
+          <AnimatedLetters letterClass ={letterClass} strArray={jobArray} idx={22} />
+        </h1>
+        <h2>🖥️Full Stack Developer HTML | CSS | Javascript | Typescript | React | Node.js | MongoDB | Software Developer </h2>
+        <Link to="/contact" className="flat-button">
+          Contact Me
+        </Link>
       </div>
+      <Logo/>
+    </div>
   )
 }
 export default Home
